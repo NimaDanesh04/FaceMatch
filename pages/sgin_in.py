7f5d1c79afffb43240573ec22547c7a089a8aa58
RaSa <nimeabcd1127@gmail.com>
2024-04-16 11:42:37 +0330

    change sign in file

--- pages/sgin_in.py
+++ pages/sgin_in.py
@@ -1,15 +1,18 @@
-from hashlib import sha256
 import streamlit as st
-import sqlite3 as sql
-import re
-
-st.set_page_config(page_title='my web app', page_icon=":tada", layout='wide')
-
 if 'key' in st.session_state:
     if st.session_state['key']:
         st.success(f'your in your accont {st.session_state["name"]} {st.session_state["last_name"]}')
 else:
     st.header('sgin in')
+    
+from hashlib import sha256
+
+import sqlite3 as sql
+import re
+
+st.set_page_config(page_title='my web app', page_icon=":tada", layout='wide')
+
+
 
     name = st.text_input('please enter your name')
     last_name = st.text_input('please enter your lasn name')
